6c1c0e8bad1f078d326431415cdacd752d6e717a
marja910 <kuleshova910@gmail.com>
2023-02-20 22:17:04 +0300

    replace script js

--- index1.js
+++ index1.js
@@ -7,12 +7,12 @@ console.log(showMessage("Я учу JavaScript!"));
 
 //Задание 2 с котиками
 
+const image = document.getElementById('cat');
+
 function changeImg() {
-    let image = document.getElementById('cat');
     image.src = "images/cat1.jpg";
 }
 
 function changeImg2() {
-    let image = document.getElementById('cat');
     image.src = "images/cat2.jpg";
 }
